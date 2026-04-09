049d9dd739d924eabf0170a563b4d5e3bb068a88
thxn <thiagoanderson707@gmail.com>
2026-04-09 00:46:54 -0300

    Create App.vue with ShoppingCart component

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <ShoppingCart />
+  </div>
+</template>
+
+<script>
+// Importando o componente que você criou
+import ShoppingCart from './components/ShoppingCart.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ShoppingCart // Registrando ele aqui
+  }
+}
+</script>
